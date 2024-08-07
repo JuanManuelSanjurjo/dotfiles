419fc27b1a059ad7396a6de0a14e3d7d1b196285
Juan Manuel Sanjurjo <57844658+JuanManuelSanjurjo@users.noreply.github.com>
2024-08-07 18:28:29 -0300

    Add files via upload

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,37 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "catppuccin-mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.default_prog = { "powershell.exe" }
+config.window_background_opacity = 0.95
+config.font_size = 14
+
+-- desabilita la barra de pestañas
+-- config.enable_tab_bar = false
+-- config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+-- You can also change the window decorations:
+config.window_decorations = "RESIZE"
+
+config.keys = {
+	{
+		key = "F11",
+		-- mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+-- 	tab_bar = {
+-- 		background = "#1e1e2e",
+-- 	},
+-- }
+-- and finally, return the configuration to wezterm
+return config
